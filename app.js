b894e86cd65081a0c3f9c8ec54e6609d389cdbe9
Noel Pena <127697218+noel-pena@users.noreply.github.com>
2023-12-17 15:49:52 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-//jshint esversion:6
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import { error } from "console";
+import bcrypt from 'bcrypt';
+// import md5 from 'md5'; if it was just hashing
 
 const app = express();
 const port = 3000;
-
+const saltRounds = 10;
 
 const db = new pg.Client({
     user: "postgres",
@@ -34,32 +35,48 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-    const username1 = req.body.username
-    const password1 = req.body.password
-    const username = await db.query("SELECT email from users WHERE email = $1",
+
+    const username1 = req.body.username;
+    // const password1 = md5(req.body.password); if this was just md5
+    const password1 = req.body.password;
+    const username = await db.query("SELECT email from users1 WHERE email = $1",
     [username1]
     );
-    const password = await db.query("SELECT password from users WHERE password = $1",
+    const password = await db.query("SELECT password from users1 WHERE password = $1",
     [password1]
     );
-    if(username1 === username.rows[0].email && password1 === password.rows[0].password){
+
+    //bcrypt use to compare 
+    // bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
+    //     // result == true
+    // });
+
+    if (username1 === username.rows[0] && password1 === password.rows[0]){
     res.render("secrets.ejs")
     } else { console.log("Account does not exist")}
 });
 
-app.post("/register", async (req, res) => {
-    const username = req.body.username
-    const password = req.body.password
-    const newLogin = await db.query("INSERT INTO users (email, password) values ($1, $2)",
-    [username, password]
-    );
-    try {
-        newLogin
-    } catch (error){
-        console.error(error);
-    };
-    res.render("secrets.ejs")
-})
+app.post("/register", (req, res) => {
+    const username = req.body.username;
+    // const password = md5(req.body.password); if you used md5
+    const password = req.body.password;
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        // The general code to push the login info goes inside of the bcrypt function
+        const newLogin = db.query("INSERT INTO users1 (email, password) values ($1, $2)",
+        [username, hash]
+        );
+        try {
+            newLogin
+        } catch (error){
+            console.error(error);
+        };
+        res.render("secrets.ejs")
+    });
+});
+
+app.get("/logout", (req, res) => {
+    res.redirect("/")
+});
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
